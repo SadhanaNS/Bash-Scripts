340c5f649fe97e33fed154fe5e40e026043fa28c
Ubuntu <adminuser@linuxvm.3uwtmbunt5puzd3yabylozmyse.ax.internal.cloudapp.net>
2026-01-18 08:09:18 +0000

    edit port check script

--- port-checker.sh
+++ port-checker.sh
@@ -4,7 +4,7 @@ HOST="localhost"
 PORTS=(80 443  5432)
 
 for port in "${PORTS[@]}" ; do
-	if nc -z "$HOST" "$port" ; then
+	if nc -4 -z -w 2  "$HOST" "$port" ; then
 		echo "Port $port is OPEN"
 	else
 		echo "Port $port is CLOSED"
